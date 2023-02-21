e9296ff4b1b7b9e1bac2c6889982f58decbc3869
Sam W <sam@sammymayhem.com>
2023-02-21 13:46:18 -0800

    setup background image and tint

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import './styles/Header.css';
+
+function Header() {
+    return (
+        <div className='header'>
+
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import './styles/Home.css';
+
+function Home() {
+
+    return (
+        <div className='home'>
+
+        </div>
+    );
+}
+
+export default Home;
